46e4eebaa9e34264771009ab5939512ad8491e31
Adelson Guimarães Monteiro <adelsonguimaraes@gmail.com>
2019-09-01 13:59:20 -0400

    Atualização Aula 6 - Grupos de Styles - React Native Básico

--- App.js
+++ App.js
@@ -1,39 +1,32 @@
 import React, { Component } from 'react'; // importando biblioteca React
-import { View, Text, Image } from 'react-native'; // importando componentes React-Native
-
-class Janta extends Component {
-
-  constructor(props) {
-    super(props); // poder usar as props
-    this.state = {comida: this.props.comida};
-    let comidas = ['Pizza', 'Lasanha', 'Burger', 'Sopa', 'Arroz'];
-
-    setInterval(()=>{
-      this.setState(previousState=>{
-        let n = Math.floor(Math.random() * comidas.length);
-        return {comida: comidas[n]};
-      });
-    }, 1000);
-  }
-
-  render() {
-    return(
-      <View>
-        <Text style={{textAlign:'center', fontWeight:'bold', fontSize:20, color:'red'}}>Hoje você vai jantar:</Text>
-        <Text style={{textAlign:'center', fontSize:20}}>{this.state.comida}</Text>
-      </View>
-    );
-  }
-}
+import { View, Text, StyleSheet } from 'react-native'; // importando componentes React-Native
 
 export default class PrimeiroProjeto extends Component {
   render() { // função de renderização para um componente component
 
     return (
       
-      <View style={{paddingTop:20}}> 
-        <Janta comida='Café'/>
+      <View style={styles.padrao}> 
+        <Text style={styles.azulGrande}>Esse é o texto 1</Text>
+        <Text style={styles.vermelho}>Esse é o texto 2</Text>
+        <Text style={[styles.azulGrande, styles.vermelho]}>Esse é o texto 3</Text>
+        <Text>Esse é o texto 4</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  padrao: {
+    paddingTop: 20
+  },
+  azulGrande: {
+    color: '#0000FF',
+    fontSize: 30,
+    textAlign: 'center'
+  },
+  vermelho: {
+    color: '#FF0000',
+    fontSize: 15
+  }
+});
