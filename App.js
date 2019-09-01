4130fa12020ac6dc8d988d65c668413023d34860
Adelson Guimarães Monteiro <adelsonguimaraes@gmail.com>
2019-09-01 09:54:27 -0400

    Atualização Aula 4 - Criando Própios Componentes - React-Native

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'; // importando biblioteca React
 import { View, Text, Image } from 'react-native'; // importando componentes React-Native
 
+class Imagem extends Component {
+  render() {
+
+    let imagem = {
+      uri: 'https://www.adelsonguimaraes.com.br/incubus/libs/img/icons/'+this.props.nome
+    }
+
+    return (
+      <Image source={imagem} style={{width:parseInt(this.props.largura), height:parseInt(this.props.altura)}} />
+    );
+  }
+}
+
 export default class PrimeiroProjeto extends Component {
   render() { // função de renderização para um componente component
-
-    // criando variável com uri da imagem a ser inserida
-    let imagem = {uri: 'https://www.adelsonguimaraes.com.br/incubus/libs/img/icons/icon-512x512.png'};
 
     return (
       
@@ -14,7 +24,8 @@ export default class PrimeiroProjeto extends Component {
         <Text>Olá Mundo</Text>
         <Text style={{fontSize:25, color:"red", margin: 20}}>Olá Mundo</Text>
 
-        <Image source={imagem} style={{width:300, height:300}}/>
+        <Imagem nome='icon-512x512.png' largura='250' altura='250' />
+
         {/* <Text> componente para mostrar um texto na tela */}
       </View>
     );
