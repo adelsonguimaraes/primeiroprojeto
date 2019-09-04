39c60d6bb694202d9f9ed67a9f0c25513efe7e1c
Adelson Guimarães Monteiro <adelsonguimaraes@gmail.com>
2019-09-04 16:43:20 -0400

    Atualização - Aula 8 - Flexbox e Alinhamentos - React Native Básico

--- App.js
+++ App.js
@@ -6,10 +6,8 @@ export default class PrimeiroProjeto extends Component {
 
     return (
       
-      <View style={{flex:1, backgroundColor: 'black'}}> 
-        <View style={{height:70, backgroundColor: 'blue'}}></View>
-        <View style={{flex:1, backgroundColor: 'green'}}></View>
-        <View style={{height:50, backgroundColor: 'yellow'}}></View>
+      <View style={{flex:1, flexDirection: "row", justifyContent: "center", alignItems: "center"}}> 
+        <View style={{width:200, height:100, backgroundColor: 'blue'}}></View>
       </View>
     );
   }
